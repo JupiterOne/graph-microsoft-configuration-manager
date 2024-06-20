8c272545e91ea78b04675dbde8d08174cd13c937
Gaston Yelmini <gastonyelmini@gmail.com>
2024-06-20 15:43:13 -0300

    INT-11148: paginate all SQL queries

--- src/steps/applications/index.test.ts
+++ src/steps/applications/index.test.ts
@@ -7,9 +7,15 @@ import { applicationRecords } from '../../../test/mockData';
 test('fetch-applications', async () => {
   jest.mock('mssql');
   sql.connect = jest.fn().mockResolvedValue({
-    query: jest.fn().mockResolvedValue({
-      recordset: applicationRecords,
-      recordsets: [0, 0],
+    query: jest.fn((query) => {
+      const match = query.match(/BETWEEN (\d+) AND (\d+)/);
+      if (!match) throw new Error('Invalid query');
+
+      const start = parseInt(match[1]);
+      const end = parseInt(match[2]);
+
+      const recordset = applicationRecords.slice(start - 1, end); // Adjust indexes for 1-based SQL row numbers
+      return Promise.resolve({ recordset, recordsets: [0, 0] });
     }),
     close: jest.fn(),
   });
--- src/steps/localUsers/index.test.ts
+++ src/steps/localUsers/index.test.ts
@@ -6,22 +6,28 @@ import { deviceRecords, localUserRecords } from '../../../test/mockData';
 
 test('fetch-local-users', async () => {
   jest.mock('mssql');
-  sql.connect = jest
-    .fn()
-    .mockResolvedValueOnce({
-      query: jest.fn().mockResolvedValue({
-        recordset: deviceRecords,
-        recordsets: [0, 0],
-      }),
-      close: jest.fn(),
-    })
-    .mockReturnValueOnce({
-      query: jest.fn().mockResolvedValue({
-        recordset: localUserRecords,
-        recordsets: [0, 0],
+  let callCount = 0;
+  sql.connect = jest.fn().mockImplementation(() => {
+    callCount++;
+    return Promise.resolve({
+      query: jest.fn((query) => {
+        const match = query.match(/BETWEEN (\d+) AND (\d+)/);
+        if (!match) throw new Error('Invalid query');
+
+        const start = parseInt(match[1]);
+        const end = parseInt(match[2]);
+
+        let recordset;
+        if (callCount === 1) {
+          recordset = deviceRecords.slice(start - 1, end);
+        } else if (callCount === 2) {
+          recordset = localUserRecords.slice(start - 1, end);
+        }
+        return Promise.resolve({ recordset, recordsets: [0, 0] });
       }),
       close: jest.fn(),
     });
+  });
 
   const stepConfig = buildStepTestConfigForStep(Steps.FETCH_LOCAL_USERS);
   const stepResult = await executeStepWithDependencies(stepConfig);
